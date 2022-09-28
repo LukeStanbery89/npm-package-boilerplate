e03e44bf9a2d3c569ccf3acdfebf0a1dae559ed1
Luke Stanbery <lukestanbery89@gmail.com>
2022-09-27 19:05:30 -0500

    add pre-commit hook

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+function $4fa36e821943b400$var$helloWorld() {
+    return "Hello, World!";
+}
+module.exports = {
+    helloWorld: $4fa36e821943b400$var$helloWorld
+};
+
+
+//# sourceMappingURL=index.js.map
